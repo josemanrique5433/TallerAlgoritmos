9112f1cb6e512168b630c1b4b3d8c4c99fb33552
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-02-19 22:48:41 -0500

    Algoritmo de operaciones matemáticas

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Ingresar por pantalla 2 números y calcular las 4 operaciones aritméticas con dichos números.
+//Imprimir en pantalla el resultado de cada operación  
+
+int num1, num2, sum, rest;
+double prod, coc;
+
+Console.WriteLine("Ingresa el primer número");
+
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingresa el segundo número diferente de 0");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+sum = num1 + num2;
+rest = num1 - num2;
+prod = num1 * num2;
+coc = num1 / num2;
+
+Console.WriteLine("La suma de " + num1 + "+" + num2 + "=" + sum);
+Console.WriteLine("La resta de " + num1 + "-" + num2 + "=" + rest);
+Console.WriteLine("La Multiplicación de " + num1 + "x" + num2 + "=" + prod);
+Console.WriteLine("La división de " + num1 + "/" + num2 + "=" + coc);
